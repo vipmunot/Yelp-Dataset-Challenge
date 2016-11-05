e57fb86c13d07b42ab4ed49d3a4b8dec227a9735
Vipul Munot <vipmunot@gmail.com>
2016-11-05 16:45:42 -0400

    update

--- Task 1.py
+++ Task 1.py
@@ -3,13 +3,32 @@
 Created on Sat Nov  5 13:09:04 2016
 
 @author: Vipul Munot
+Link: https://github.com/karenxiao/pandas-express
 """
-from pymongo import MongoClient
+import json
 import pandas as pd
-client =  MongoClient()
-db = client.Yelp
-collection = db.review
-print ("Total Rows: ",collection.count())
-for row in collection.find():
-    data = pd.DataFrame(row)
-print(data['stars'].value_counts())
+def load_data(filepath):
+    data = []
+    with open(filepath) as file:
+        for line in file:
+            data.append(json.loads(line.rstrip()))
+    return data
+
+
+def main():
+    business = pd.DataFrame.from_dict(load_data("E:/IUB/Search/Project/business.json"))
+    # Create dummy variables for categories
+    categories_df = business['categories'].str.join(sep=',').str.get_dummies(sep=',')
+    # Save the list of categories for future use
+    categories = categories_df.columns.values
+    # Merge it with our original dataframe
+    business = pd.merge(business, categories_df, left_index = True, right_index = True)
+    #Instead of dropping the categories column, we're going to keep it around, but reformat it as a tuple
+    business['categories'] = business['categories'].apply(lambda x: tuple(x))
+    print(business['categories'])
+#    print(business[business['Chinese'] == 1].head())
+#    print(business['Chinese'].sum())
+#    review = pd.DataFrame.from_dict(load_data("E:/IUB/Search/Project/review.json"))
+
+if __name__ == '__main__':
+	main()
